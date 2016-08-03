6ae27e8381c90f4b511f15df29dbd218128f6a91
Dustin Ruetz <dustinruetz@gmail.com>
2016-08-03 18:03:07 -0400

    gulpfile updated to use ES6 features

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 // required npm modules
-var gulp = require("gulp");
-// documentation modules
-var doctoc = require("gulp-doctoc");
-var marked = require("gulp-marked");
+const gulp = require("gulp"),
+		doctoc = require("gulp-doctoc"),
+		inject = require("gulp-inject"),
+		marked = require("gulp-marked");
 
 // default tasks to run on "gulp" command
-gulp.task("default", ["docTasks", "readmeTOC"], function() {
+gulp.task("default", ["docTasks", "readmeTOC"], () => {
 	// files to watch and tasks to run
 	gulp.watch("dev/*.md", ["docTasks"]);
 	gulp.watch("readme.md", ["readmeTOC"]);
@@ -15,7 +15,7 @@ gulp.task("default", ["docTasks", "readmeTOC"], function() {
 // 1) pipes all MD into doctoc
 // 2) doctoc generates TOC, then pipes into marked
 // 3) marked converts MD to HTML, then pipes into destination
-gulp.task("docTasks", function() {
+gulp.task("docTasks", () => {
 	return gulp.src("dev/*.md")
 		.pipe(doctoc())
 		.pipe(marked())
@@ -25,7 +25,7 @@ gulp.task("docTasks", function() {
 // readmeTOC
 // 1) pipes readme.md into doctoc
 // 2) doctoc generates TOC, then pipes into destination
-gulp.task("readmeTOC", function() {
+gulp.task("readmeTOC", () => {
 	return gulp.src("readme.md")
 		.pipe(doctoc())
 		.pipe(gulp.dest(""));
